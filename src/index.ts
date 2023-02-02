611eca67101051cac9b193a2df3a887cf238c351
JJetmar <josef.jetmar@apify.com>
2023-02-02 15:58:49 +0100

    Added new methods.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,53 @@
 import Apify, { Actor } from 'apify';
+import Crawlee from 'crawlee';
 
-// Hack for support libraries for using older version of Apify SDK
 declare module 'apify' {
-    export const openKeyValueStore: typeof Actor.openKeyValueStore;
-    export const isAtHome: typeof Actor.isAtHome;
+    // Methods
+    export const call: typeof Actor.call;
+    export const createProxyConfiguration: typeof Actor.createProxyConfiguration;
     export const getEnv: typeof Actor.getEnv;
+    export const getInput: typeof Actor.getInput;
+    export const getValue: typeof Actor.getValue;
+    export const isAtHome: typeof Actor.isAtHome;
+    export const openDataset: typeof Actor.openDataset;
+    export const openKeyValueStore: typeof Actor.openKeyValueStore;
+    export const openRequestQueue: typeof Actor.openRequestQueue;
+    export const pushData: typeof Actor.pushData;
+    export const setValue: typeof Actor.setValue;
+    export const utils: typeof Crawlee.utils;
+
+    // Events
+    export const events: {
+        on: typeof Actor.on
+        off: typeof Actor.off
+    }
+
+    // Crawlers
+    export const PuppeteerCrawler: typeof Crawlee.PuppeteerCrawler;
 }
 
+// Hack for support libraries for using older version of Apify SDK
 Object.assign(Apify, {
-    openKeyValueStore: Actor.openKeyValueStore,
+    // Methods
+    call: Actor.call,
+    createProxyConfiguration: Actor.createProxyConfiguration,
+    getEnv: Actor.getEnv,
+    getInput: Actor.getInput,
+    getValue: Actor.getValue,
     isAtHome: Actor.isAtHome,
-    getEnv: Actor.getEnv
+    openDataset: Actor.openDataset,
+    openKeyValueStore: Actor.openKeyValueStore,
+    openRequestQueue: Actor.openRequestQueue,
+    pushData: Actor.pushData,
+    setValue: Actor.setValue,
+    utils: Crawlee.utils,
+
+    // Events
+    events: {
+        on: Actor.on,
+        off: Actor.off
+    },
+
+    // Crawlers
+    PuppeteerCrawler: Crawlee.PuppeteerCrawler,
 });
--- test/apify-sdk-legacy.test.ts
+++ test/apify-sdk-legacy.test.ts
@@ -4,30 +4,123 @@ jest.mock('apify', () => {
     return {
         ...originalModule,
         Actor: {
-            openKeyValueStore: jest.fn(),
+            call: jest.fn(),
+            createProxyConfiguration: jest.fn(),
+            getEnv: jest.fn(),
+            getInput: jest.fn(),
+            getValue: jest.fn(),
             isAtHome: jest.fn(),
-            getEnv: jest.fn()
+            openKeyValueStore: jest.fn(),
+            openDataset: jest.fn(),
+            openRequestQueue: jest.fn(),
+            pushData: jest.fn(),
+            setValue: jest.fn(),
+            on: jest.fn(),
+            off: jest.fn(),
         }
     }
 });
 
+jest.mock('crawlee', () => {
+    const originalModule = jest.requireActual('crawlee');
+
+    return {
+        ...originalModule,
+        utils: {
+            log: {
+                setLevel: jest.fn()
+            },
+            sleep: jest.fn()
+        },
+        PuppeteerCrawler: jest.fn()
+    }
+});
+
 import '../src/index';
-import Apify from 'apify';
+import Apify, { Actor } from "apify";
+import Crawlee from "crawlee";
 
 describe("Testing Apify legacy", () => {
 
-    test("Calling Apify.openKeyValueStore should call Actor.openKeyValueStore", () => {
-        Apify.openKeyValueStore("test");
-        expect(Apify.Actor.openKeyValueStore).toBeCalledTimes(1);
+    test("Calling Apify.call should call Actor.call", () => {
+        Apify.call("actorName");
+        expect(Apify.Actor.call).toBeCalledTimes(1);
     });
 
-    test("Calling Apify.isAtHome: Actor.isAtHome", () => {
+    test("Calling Apify.createProxyConfiguration should call Actor.createProxyConfiguration", () => {
+        Apify.createProxyConfiguration();
+        expect(Apify.Actor.createProxyConfiguration).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.getEnv should call Actor.getEnv", () => {
+        Apify.getEnv();
+        expect(Apify.Actor.getEnv).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.getInput should call Actor.getInput", () => {
+        Apify.getInput();
+        expect(Apify.Actor.getInput).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.getValue should call Actor.getValue", () => {
+        Apify.getValue("valueKey");
+        expect(Apify.Actor.getValue).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.isAtHome should call Actor.isAtHome", () => {
         Apify.isAtHome();
         expect(Apify.Actor.isAtHome).toBeCalledTimes(1);
     });
 
-    test("Calling Apify.getEnv: Actor.getEnv", () => {
-        Apify.getEnv();
-        expect(Apify.Actor.getEnv).toBeCalledTimes(1);
+    test("Calling Apify.openDataset should call Actor.openDataset", () => {
+        Apify.openDataset();
+        expect(Apify.Actor.openDataset).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.openKeyValueStore should call Actor.openKeyValueStore", () => {
+        Apify.openKeyValueStore();
+        expect(Apify.Actor.openKeyValueStore).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.openRequestQueue should call Actor.openRequestQueue", () => {
+        Apify.openRequestQueue();
+        expect(Apify.Actor.openRequestQueue).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.pushData should call Actor.pushData", () => {
+        Apify.pushData({});
+        expect(Apify.Actor.pushData).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.setValue should call Actor.setValue", () => {
+        Apify.setValue("key", "value");
+        expect(Actor.setValue).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.utils.log should call Crawlee.utils.log", () => {
+        Apify.utils.log.setLevel(1);
+        expect(Crawlee.utils.log.setLevel).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.utils.sleep should call Crawlee.utils.sleep", () => {
+        Apify.utils.sleep();
+        expect(Crawlee.utils.sleep).toBeCalledTimes(1);
+    });
+
+    // Events
+    test("Calling Apify.events.on should call Actor.on", () => {
+        Apify.events.on('persistState', () => {});
+        expect(Actor.on).toBeCalledTimes(1);
+    });
+
+    test("Calling Apify.events.off should call Actor.off", () => {
+        Apify.events.off('persistState', () => {});
+        expect(Actor.off).toBeCalledTimes(1);
+    });
+
+    // Crawlers
+    test("Calling Apify.PuppeteerCrawler.off should call Actor.off", () => {
+        new Apify.PuppeteerCrawler();
+        expect(Crawlee.PuppeteerCrawler).toBeCalledTimes(1);
     });
 });
